4a21735a4e92ea434a6940a309e770c07bf154f5
Carl Zornes <czornes@phunware.com>
2015-02-05 14:27:02 -0800

    Video TVAST fixes. Z

--- Sample/MaaSAdvertising.framework/Versions/A/Headers/PWAdsConstants.h
+++ Sample/MaaSAdvertising.framework/Versions/A/Headers/PWAdsConstants.h
@@ -10,7 +10,7 @@
 #ifndef PWAds_iOS_Sample_PWAdsConstants_h
 #define PWAds_iOS_Sample_PWAdsConstants_h
 
-#define PWADS_VERSION @"3.1.1"
+#define PWADS_VERSION @"3.1.2"
 
 /**
  `PWAdsAdType` defines the available ad types for interstitial ads.
