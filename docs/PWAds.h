b44da3f4cb699be23888d7aba626ce3db135f25d
Carl Zornes <czornes@phunware.com>
2015-03-17 11:18:56 -0700

    Documentation updates for MaaSAdvertising SDK. Z

---
+++ docs/PWAds.h
@@ -0,0 +1,19 @@
+//
+//  PWAds.h
+//  PWAds-iOS-Sample
+//
+//  Created by Nick Penteado on 4/11/12.
+//  Copyright (c) 2012 PWAds!. All rights reserved.
+//
+
+#ifndef PWAds_iOS_SDK_h
+#define PWAds_iOS_SDK_h
+
+#import "PWAdsConstants.h"
+#import "PWAdsAppTracker.h"
+#import "PWAdsRequest.h"
+#import "PWAdsBannerAdView.h"
+#import "PWAdsInterstitialAd.h"
+#import "PWAdsNativeAdManager.h"
+#import "PWAdsNativeAd.h"
+#endif
