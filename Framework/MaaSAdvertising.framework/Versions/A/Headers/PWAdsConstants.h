b61381caea252186a6dcfcab6f0ef2e22d007672
Carl Zornes <czornes@phunware.com>
2015-04-28 11:43:30 -0700

    Video fixes. Z

--- Framework/MaaSAdvertising.framework/Versions/A/Headers/PWAdsConstants.h
+++ Framework/MaaSAdvertising.framework/Versions/A/Headers/PWAdsConstants.h
@@ -10,7 +10,7 @@
 #ifndef PWAds_iOS_Sample_PWAdsConstants_h
 #define PWAds_iOS_Sample_PWAdsConstants_h
 
-#define PWADS_VERSION @"3.1.5"
+#define PWADS_VERSION @"3.1.6"
 
 /**
  `PWAdsAdType` defines the available ad types for interstitial ads.
