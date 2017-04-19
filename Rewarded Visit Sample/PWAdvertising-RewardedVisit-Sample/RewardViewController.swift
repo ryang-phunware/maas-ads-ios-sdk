6ca5029266d561c2920af7ff8d5c27d391cfae65
Rongbo Yang <ryang@phunware.com>
2017-04-19 11:12:36 -0500

    Update PWAdvertising to 3.6.1. See CHANGELOG for details.

---
+++ Rewarded Visit Sample/PWAdvertising-RewardedVisit-Sample/RewardViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RewardViewController.swift
+//  PWAdvertising
+//
+//  Created by Rongbo Yang on 4/17/17.
+//  Copyright © 2017 Phunware, Inc. All rights reserved.
+//
+
+import UIKit
+
+class RewardViewController: UIViewController {
+
+    var message: PWMSGZoneMessage? = nil
+    let queue = DispatchQueue.global(qos: .userInteractive)
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var rewardImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let msg = message else {
+            return
+        }
+        
+        titleLabel.text = msg.alertTitle
+        bodyLabel.text = msg.alertBody
+        
+        if let imageURLString = msg.metaData["imageURL"] as? String {
+            queue.async {
+                if let url = URL(string: imageURLString), let imageData = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        self.rewardImageView.image = UIImage(data: imageData)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Rewarded Visit Sample/PWAdvertising-RewardedVisit-Sample/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  PWAdsRewardedVisitSample
+//
+//  Created by Rongbo Yang on 3/8/17.
+//  Copyright © 2017 Phunware, Inc. All rights reserved.
+//
+
+import UIKit
+
+let zoneId = "78393"
+let userId = "123456"
+
+class ViewController: UIViewController {
+    
+    var videoIsPreCached = false
+    let adRequest = PWAdsRequest(zoneID: zoneId)
+    var rewardedVideo = PWAdsRewardedVideo()
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var logView: UITextView!
+
+    // MARK: - IBActions
+    
+    @IBAction func loadButtonPressed(_ sender: UIButton) {
+        adRequest?.userID = userId
+        rewardedVideo.load(adRequest)
+        
+        logView.text = ""
+        logView.insertText("Loading ad......\n")
+    }
+    
+    @IBAction func play(_ sender: UIButton) {
+        playButton.isEnabled = false
+        rewardedVideo.present(from: self)
+    }
+    
+    // MARK: - View Controller Lifecyles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        rewardedVideo.delegate = self
+        playButton.isEnabled = false
+    }
+}
+
+extension ViewController: PWAdsRewardedVideoDelegate {
+    func rewardedVideoDidLoadAd(_ rewardedVideo: PWAdsRewardedVideo!, withAdExtensionData adExtensionData: [AnyHashable : Any]!) {
+        logView.insertText("Caching video......\n")
+    }
+    
+    func rewardedVideoDidFinishedPreCaching(_ rewardedVideo: PWAdsRewardedVideo!, withAdExtensionData adExtensionData: [AnyHashable : Any]!) {
+        videoIsPreCached = true
+        logView.insertText("Ready to play!\n")
+        playButton.isEnabled = true
+    }
+    
+    func rewardedVideoWillDismissModal(_ rewardedVideo: PWAdsRewardedVideo!, withAdExtensionData adExtensionData: [AnyHashable : Any]!) {
+    }
+    
+    func rewardedVideoDidEndPlaybackSuccessfully(_ rewardedVideo: PWAdsRewardedVideo!, withRVResponseObject customData: [AnyHashable : Any]!, andAdExtensionData adExtensionData: [AnyHashable : Any]!) {
+        logView.insertText("RV Success!\n")
+    }
+}
