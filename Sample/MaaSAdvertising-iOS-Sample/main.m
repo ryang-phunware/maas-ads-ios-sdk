c14f72dba6ace46a82b73eb2c3b2fe112be5ee52
Rongbo Yang <ryang@phunware.com>
2017-05-07 10:32:39 -0500

    Adopt CocoaPods in sample app

--- Sample/MaaSAdvertising-iOS-Sample/main.m
+++ Sample/MaaSAdvertising-iOS-Sample/main.m
@@ -2,16 +2,14 @@
 //  main.m
 //  MaaSAdvertising-iOS-Sample
 //
-//  Created by Carl Zornes on 12/2/13.
-//  Copyright (c) 2013 Phunware. All rights reserved.
+//  Created on 5/6/17.
+//  Copyright © 2017 Phunware, Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
