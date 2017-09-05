2c94a87aafbcf7845edb3888a68d74f379a9de1f
Rongbo Yang <ryang@phunware.com>
2017-09-05 15:31:23 -0500

    Update the SDK Version

--- Framework/PWAdvertising.framework/Headers/PWAdvertising.h
+++ Framework/PWAdvertising.framework/Headers/PWAdvertising.h
@@ -22,7 +22,7 @@
 #import "PWAdsVideoBase.h"
 #import <PWCore/PWCore.h>
 
-static NSString * const PWAdvertisingVersion = @"3.6.3.17";
+static NSString * const PWAdvertisingVersion = @"3.6.3";
 
 @interface PWAdvertising : NSObject
 
